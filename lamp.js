a10cd52f6dd2097aad40bc6e6f55dca760d27e8c
Tiefighter450 <74991102+Tiefighter450@users.noreply.github.com>
2021-06-12 08:24:04 -0500

    Add files via upload

---
+++ lamp.js
@@ -0,0 +1,34 @@
+status = false;
+objects = [];
+percent = 0;
+img = "";
+function back() {
+  window.location.href = "index.html";
+}
+function preload() {
+  img = loadImage("lamp.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(400, 400);
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!");
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if(error) {
+      console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw() {
+  canvas.center();
+  image(img, 0, 0, 400, 400);
+}
---
+++ tv.js
@@ -0,0 +1,34 @@
+status = false;
+objects = [];
+percent = 0;
+img = "";
+function back() {
+  window.location.href = "index.html";
+}
+function preload() {
+  img = loadImage("tv.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(500, 400);
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!");
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if(error) {
+      console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw() {
+  canvas.center();
+  image(img, 0, 0, 500, 400);
+}
